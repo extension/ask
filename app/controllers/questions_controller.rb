3f7b924e91df93fe550e821dab625a3b881eb166
benmac <ben.macneill@gmail.com>
2012-06-07 00:37:08 -0400

    Misc placeholder elements and layouts
    - renamed the application layout to expert to reflect purpose
    - added various question view elements for expert view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,6 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find_by_id(params[:id])
     @question_responses = @question.responses
+    @fake_related = Question.find(:all, :limit => 3, :offset => rand(Question.count))
   end
 end
