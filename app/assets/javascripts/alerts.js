c5a7fddf2754e527cc2e5a8e51b850759e392a3a
Jason Adam Young <jasonadamyoung@gmail.com>
2012-06-21 15:10:26 -0400

    let's wrap our functions right, shall we?

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -1,10 +1,10 @@
-$(function() 
+$(function() {
    $('#flash_notification').hide().delay(500).slideDown('slow', function() {
       $(this).delay(4000).fadeOut();
    });
 
    $("#flash_notification").live('click', function(){
-       $(this).stop().fadeOut();;
+       $(this).stop().fadeOut();
    });
 
 });
