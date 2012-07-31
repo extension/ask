d2af436dd937a08aed06b656437d822ccdc7b663
benmac <ben.macneill@gmail.com>
2012-07-31 11:26:40 -0400

    Some design for the groups page
    - random placeholder images for group avatars
    - display group avatar wherever group is listed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,7 +26,7 @@ module ApplicationHelper
   def get_avatar(user, image_size = :medium)
     case image_size
         when :medium    then image_size_in_px = "100x100"
-        when :thumb     then image_size_in_px = "50x50"
+        when :thumb     then image_size_in_px = "40x40"
     end
     
     if user.avatar.present? 
@@ -37,6 +37,17 @@ module ApplicationHelper
     end
     
     return link_to(return_string, expert_user_path(user.id), {:title => user.name}).html_safe  
+  end
+  
+  def get_group_avatar(group, image_size = :medium)
+    case image_size
+        when :medium    then image_size_in_px = "100x100"
+        when :thumb     then image_size_in_px = "40x40"
+    end
+    # if no avatar, show a random one each time
+    return_string = image_tag("group_avatar_placeholder_0" + rand(1..3).to_s + ".png", :size => image_size_in_px)
+    
+    return link_to(return_string, expert_group_path(group.id), {:title => group.name}).html_safe  
   end
   
   def default_location_id(fallback_location_id = nil)
