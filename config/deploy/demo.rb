09a23e1ce63a2439b91645394bbca901139fc109
sdnall <s.daniel.nall@gmail.com>
2012-08-28 10:48:06 -0400

    bring back REBUILD=true param when deploying

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -5,3 +5,7 @@ else
   set :branch, 'staging'
 end
 server 'demo.ask.extension.org', :app, :web, :db, :primary => true
+
+if(ENV['REBUILD'] == 'true')
+  after "deploy:update_code", "db:rebuild"
+end
--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -5,3 +5,7 @@ else
   set :branch, 'development'
 end
 server 'dev.ask.extension.org', :app, :web, :db, :primary => true
+
+if(ENV['REBUILD'] == 'true')
+  after "deploy:update_code", "db:rebuild"
+end
