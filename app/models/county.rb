eb3f63cc2490527f33ee5117200f38224c80d774
benmac <ben.macneill@gmail.com>
2012-09-27 17:16:12 -0400

    method to distinguish counties from incorporated areas

--- app/models/county.rb
+++ app/models/county.rb
@@ -4,5 +4,14 @@ class County < ActiveRecord::Base
   has_many :group_counties
   has_many :groups, :through => :group_counties
   belongs_to :location
+  
+  def name
+    if self.censusclass == "C7"
+      # return self.name + " (Non-county incorporation)"
+      return self[:name]
+    else
+      return self[:name] + " County"
+    end
+  end
     
 end
