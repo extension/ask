adfa0827f91aa4a8d0fa96cb7966339aa3723663
benmac <ben.macneill@gmail.com>
2012-10-22 17:27:53 -0400

    Don't rewrite private URLs.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,10 +4,6 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find_by_id(params[:id])
     
-    if @question.is_private?
-      redirect_to home_private_page_url
-    end
-    
     @question_responses = @question.responses
     @fake_related = Question.public_visible.find(:all, :limit => 3, :offset => rand(Question.public_visible.count))
   end
