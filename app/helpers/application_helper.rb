86cb81bc42894b670375669c0adf251433f599e3
Jason Adam Young <jayoung@extension.org>
2012-10-23 21:18:23 -0400

    split off location helpers (better org. / minimizes conflicts)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
+# === COPYRIGHT:
+#  Copyright (c) North Carolina State University
+#  Developed with funding for the National eXtension Initiative.
+# === LICENSE:
+#  BSD(-compatible)
+#  see LICENSE file
+
 module ApplicationHelper
   # TODO: Need to fix this
   def format_text_for_display(content)
@@ -90,43 +97,4 @@ module ApplicationHelper
     return return_string
   end
 
-
-  def default_location_id(fallback_location_id = nil)
-    if(current_location.present?)
-      current_location.id
-    elsif(fallback_location_id.present?)
-      fallback_location_id
-    end
-  end
-
-
-  def default_county_id(fallback_county_id = nil)
-    if(current_county.present?)
-      current_county.id
-    elsif(fallback_county_id.present?)
-      fallback_county_id
-    end
-  end
-
-  def get_location_options
-    locations = Location.find(:all, :order => 'entrytype, name')
-    return [['', '']].concat(locations.map{|l| [l.name, l.id]})
-  end
-
-  def get_county_options(provided_location = nil)
-    if params[:location_id] and params[:location_id].strip != '' and location = Location.find(params[:location_id])
-      counties = location.counties.find(:all, :order => 'name', :conditions => "countycode <> '0'")
-      return ([['All counties', 'All counties']].concat(counties.map{|c| [c.name, c.id]}))
-    elsif(provided_location)
-      counties = provided_location.counties.find(:all, :order => 'name', :conditions => "countycode <> '0'")
-      return ([['All counties', 'All counties']].concat(counties.map{|c| [c.name, c.id]}))
-    end
-  end
-
-  def display_location_and_county
-    locations = []
-    locations  << current_county.name if current_county
-    locations  << (current_location.nil? ? 'Unknown' : current_location.name)
-    locations.join(', ').html_safe
-  end
 end
