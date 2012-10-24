527c247fe4088bb3312792c327302606160b1330
Jason Adam Young <jayoung@extension.org>
2012-10-23 21:16:53 -0400

    cache lib

---
+++ lib/cache_tools.rb
@@ -0,0 +1,22 @@
+# === COPYRIGHT:
+#  Copyright (c) North Carolina State University
+#  Developed with funding for the National eXtension Initiative.
+# === LICENSE:
+#  BSD(-compatible)
+#  see LICENSE file
+
+module CacheTools
+
+  def get_cache_key(method_name,optionshash={})
+   optionshashval = Digest::SHA1.hexdigest(optionshash.inspect)
+   cache_key = "#{self.name}::#{method_name}::#{optionshashval}"
+   return cache_key
+  end
+
+  def get_instance_cache_key(identifier,method_name,optionshash={})
+   optionshashval = Digest::SHA1.hexdigest(optionshash.inspect)
+   cache_key = "#{identifier}::#{method_name}::#{optionshashval}"
+   return cache_key
+  end
+
+end
