f41ab20490c2d28706871e547717aa20d796df56
Jason Adam Young <jayoung@extension.org>
2012-10-24 22:16:32 -0400

    do the right thing with the session secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Aae::Application.config.secret_token = '95d5c845daf8bed03733cf8074010c9f8ad4b0540ccdae8ea59484f0a3d94fa692f45ea67d989b63c5d7060c7da13d6b7c2a28c6d5cd91fcefad07694ec912bd'
+Aae::Application.config.secret_token = Settings.session_token
