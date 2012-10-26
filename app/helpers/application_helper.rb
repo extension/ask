6f9735f83b68cb34871dd7bb17c183ecda3143db
benmac <ben.macneill@gmail.com>
2012-10-26 09:51:34 -0400

    Pump up the volume
    Make the badge size match the avatar size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,7 +57,7 @@ module ApplicationHelper
   end
 
   def link_expert_user_avatar(user, image_size = :medium, suppress_single = false)
-    return link_to(get_avatar_for_user(user, image_size), expert_user_path(user.id), {:title => user.name, :class => (suppress_single ? "suppress_single" : "")}).html_safe
+    return link_to(get_avatar_for_user(user, image_size), expert_user_path(user.id), {:title => user.name, :class => "#{image_size} " + (suppress_single ? "suppress_single" : "")}).html_safe
   end
 
   def link_public_group_avatar(group, image_size = :medium)
@@ -65,7 +65,7 @@ module ApplicationHelper
   end
 
   def link_expert_group_avatar(group, image_size = :medium, suppress_single = false)
-    return link_to(get_avatar_for_group(group, image_size), expert_group_path(group.id), {:title => group.name, :class => (suppress_single ? "suppress_single" : "")}).html_safe
+    return link_to(get_avatar_for_group(group, image_size), expert_group_path(group.id), {:title => group.name, :class => "#{image_size} " + (suppress_single ? "suppress_single" : "")}).html_safe
   end
 
   def get_avatar_for_group(group, image_size = :medium)
