944c4412002ca15a867587e61736e2fc506d2abb
Jason Adam Young <jayoung@extension.org>
2012-11-08 11:22:00 -0500

    reference correct variable

--- app/models/question.rb
+++ app/models/question.rb
@@ -144,7 +144,7 @@ class Question < ActiveRecord::Base
   
   def auto_assign_by_preference
     if existing_question = Question.joins(:submitter).find(:first, :conditions => ["questions.id != #{self.id} and questions.body = ? and users.email = '#{self.email}'", self.body])
-      reject_msg = "This question was a duplicate of incoming question ##{existing_sq.id}"
+      reject_msg = "This question was a duplicate of incoming question ##{existing_question.id}"
       self.add_resolution(STATUS_REJECTED, User.system_user, reject_msg)
       return
     end
