49d232c81486797610a45b38f2e0844f54003323
sdnall <s.daniel.nall@gmail.com>
2012-11-19 14:17:22 -0500

    use Settings value for daily summary delivery time

--- script/schedule_daily_summaries.rb
+++ script/schedule_daily_summaries.rb
@@ -8,4 +8,4 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 
 @mydatabase = ActiveRecord::Base.connection.instance_variable_get("@config")[:database]
 
-Notification.create(notification_type: Notification::AAE_DAILY_SUMMARY, created_by:1, recipient_id: 1, delivery_time: 2.hours.from_now)
+Notification.create(notification_type: Notification::AAE_DAILY_SUMMARY, created_by:1, recipient_id: 1, delivery_time: Settings.daily_summary_delivery_time)
