d87a75f191268301ad52e48f6f6203748a4a591c
Aaron Hundley <aaron.hundley@extension.org>
2012-12-07 16:50:04 -0500

    Try to Ensure that Taggings are Not Duplicated

--- app/models/question.rb
+++ app/models/question.rb
@@ -419,11 +419,15 @@ class Question < ActiveRecord::Base
   end
   
   def set_tag(tag)
-    if self.tags.collect{|t| t.name}.include?(Tag.normalizename(tag))
+    if self.tags.collect{|t| Tag.normalizename(t.name)}.include?(Tag.normalizename(tag))
       return false
     else 
       if(tag = Tag.find_or_create_by_name(Tag.normalizename(tag)))
-        self.tags << tag
+        begin
+          self.tags << tag
+        rescue
+          return
+        end  
         return tag
       end
     end
