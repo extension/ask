ceffe2259076e8694cda2ed18d86861f6566f96c
sdnall <s.daniel.nall@gmail.com>
2013-01-07 20:42:18 -0500

    use https for sitemaps

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://#{Settings.urlwriter_host}"
+SitemapGenerator::Sitemap.default_host = "https://#{Settings.urlwriter_host}"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
