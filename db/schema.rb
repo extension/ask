415596fc4c73882e7ce51b25057c0085d2d3895a
Aaron Hundley <aaron.hundley@extension.org>
2013-02-12 12:00:04 -0500

    Migration to Set the All County Value for Qualified Groups and Experts
    The all county county was not being set in the db for groups and
    experts when they were adding the all county option for a state in the
    location interfaces for experts and groups. Changes for code for those
    interfaces to come.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130131150507) do
+ActiveRecord::Schema.define(:version => 20130211221831) do
 
   create_table "activity_logs", :force => true do |t|
     t.integer  "user_id",                     :null => false
