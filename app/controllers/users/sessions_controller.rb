bf69f06f7e0ef904049b801a5d81d94d37bb9620
benmac <ben.macneill@gmail.com>
2013-05-21 12:03:22 -0400

    send expert-side requets directly to People sign-in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,5 +5,11 @@
 # see LICENSE file
 
 class Users::SessionsController < Devise::SessionsController
+  
+  def new
+    if session[:user_return_to].present? && (session[:user_return_to].include? '/expert/')
+      redirect_to authmap_omniauth_authorize_path(:people, :redirect_to => (params[:redirect_to].present?) ? params[:redirect_to] : session[:user_return_to])
+    end
+  end
 
 end
