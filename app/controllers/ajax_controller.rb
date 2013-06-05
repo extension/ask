abf471dbb1033093366af279070b96c5891ba549
benmac <ben.macneill@gmail.com>
2013-06-05 14:25:08 -0400

    make sure tag param is included in results if tag exists

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -14,14 +14,17 @@ class AjaxController < ApplicationController
       tags = Tag.used_at_least_once.order('created_at DESC').limit(12)
     end
     list = []
-    tag_count_description = "not used yet"
     tags.each do |t|
       list <<  Hash[ id: t.id, label: t.name, name: t.name, tag_count: "#{number_with_delimiter(t.tag_count, :delimiter => ',')}"] if t.name != search_term
-      if t.name == search_term
-        tag_count_description = t.tag_count
-      end
     end
-
+    
+    tag_count_description = "not used yet"
+    
+    param_tag = Tag.used_at_least_once.find_by_name(search_term)
+    if param_tag
+      tag_count_description = number_with_delimiter(param_tag.tag_count, :delimiter => ',')
+    end
+    
     list.unshift(Hash[id: nil, label: search_term, name: search_term, tag_count: tag_count_description])
     render json: list
   end
