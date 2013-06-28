2c7850080f882c007dba01d5513cd4f54d547744
sdnall <s.daniel.nall@gmail.com>
2013-06-28 10:11:08 -0400

    use ssl

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,7 @@
 Airbrake.configure do |config|
 		 config.api_key		 	= '654ed3f601814831fbd43fe084ea87c8'
 		 config.host				= 'apperrors.extension.org'
-		 config.port				= 80
+		 config.port				= 443
 		 config.secure			= config.port == 443
 	 end
 	 
