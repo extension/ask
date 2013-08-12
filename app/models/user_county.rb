f7b962bca44855531fc8b6bcc71819f769602bda
Aaron Hundley <aaron.hundley@extension.org>
2013-08-12 16:20:54 -0400

    Added Validations and Checks for Existing Records on Location/County Addition

--- app/models/user_county.rb
+++ app/models/user_county.rb
@@ -1,4 +1,6 @@
 class UserCounty < ActiveRecord::Base  
   belongs_to :user
   belongs_to :county
+  
+  validates :user_id, :uniqueness => { :scope => :county_id }
 end
--- app/models/user_location.rb
+++ app/models/user_location.rb
@@ -1,4 +1,6 @@
 class UserLocation < ActiveRecord::Base
   belongs_to :user
   belongs_to :location
+  
+  validates :user_id, :uniqueness => { :scope => :location_id }
 end
