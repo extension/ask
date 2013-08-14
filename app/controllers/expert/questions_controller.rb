7db358d75a91ba8af8a41d3cb28dfd320ce66ab7
Aaron Hundley <aaron.hundley@extension.org>
2013-08-14 13:30:04 -0400

    Provide Model Level Group Connection Key Validation Along With Checking Validity of Group Connections Before Saving
    Along with this is a forced format of question show on the expert side
    to avoid app errors on incorrect format types. Included is code cleanup
    on existing code as well.

--- app/controllers/expert/questions_controller.rb
+++ app/controllers/expert/questions_controller.rb
@@ -6,8 +6,8 @@
 
 class Expert::QuestionsController < ApplicationController
   layout 'expert'
-  before_filter :authenticate_user!
-  before_filter :require_exid
+  before_filter :authenticate_user!, :require_exid
+  before_filter :set_format, :only => [:show]
   
   def index
     @locations = Location.order('fipsid ASC')
