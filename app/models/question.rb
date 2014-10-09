2e3dc4db3d8d59115084a4d0f47f01b70bb7a469
Ben MacNeill <ben.macneill@gmail.com>
2014-10-09 15:30:18 -0400

    provide contextual instructions and explanation for repsonses

--- app/models/question.rb
+++ app/models/question.rb
@@ -907,6 +907,13 @@ class Question < ActiveRecord::Base
     self.status_state == STATUS_RESOLVED
   end
 
+  def needs_response?
+    self.status_state != STATUS_RESOLVED &&
+    self.status_state != STATUS_NO_ANSWER &&
+    self.status_state != STATUS_CLOSED &&
+    self.status_state != STATUS_REJECTED
+  end
+
   def create_evaluation_notification
     if(self.is_answered? and !self.submitter.answered_evaluation_for_question?(self) and self.assigned_group.send_evaluation)
       Notification.create(notifiable: self, created_by: self.submitter.id, recipient_id: self.submitter.id, notification_type: Notification::AAE_PUBLIC_EVALUATION_REQUEST, delivery_time: 1.minute.from_now )
