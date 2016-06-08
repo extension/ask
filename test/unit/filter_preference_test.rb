5955bcce9c09104b2ade7c9c33cf4062ae379cab
Jason Adam Young <jayoung@extension.org>
2016-06-08 08:25:11 -0400

    there's no testing, just don't fake it anymore

--- test/unit/filter_preference_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FilterPreferenceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/group_county_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GroupCountyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
