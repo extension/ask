69e4362bc815b8bbdb1c18a175369e8b87a5fcdc
Jason Adam Young <jayoung@extension.org>
2016-06-16 10:23:41 -0400

    constants for tracking auto assignment

--- app/models/auto_assignment_log.rb
+++ app/models/auto_assignment_log.rb
@@ -10,11 +10,35 @@ class AutoAssignmentLog < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
 
+
+  # auto assignment constants
+  LOCATION_MATCH_GROUP_IGNORES_COUNTY = 101
+  # reason_assigned = "The question location (#{self.location.name}) matched the member's expertise location. Note: This group ignores counties when automatically assigning questions."
+  COUNTY_MATCH = 102
+  # reason_assigned = "You chose to accept questions based on location (#{self.county.name})"
+  LOCATION_MATCH_ALL_COUNTY = 103
+  # reason_assigned = "You chose to accept questions based on location (all counties in #{self.location.name})"
+  ANYWHERE = 104
+  # reason_assigned = "You chose to accept questions from #{group.name} group"
+  LEADER = 105
+  # reason_assigned = "You are a group leader"
+  WRANGLER_EMPTY_GROUP = 201
+  WRANGLER_COUNTY_MATCH = 202
+  WRANGLER_LOCATION_MATCH = 203
+  WRANGLER_ANYWHERE = 204
+
+  # doh!
+  FAILURE = 13
+
+
   def self.log_assignment(log_values)
     user_pool = log_values[:user_pool].sort { |a, b| a.open_question_count <=> b.open_question_count }
     pool_floor = user_pool[0].open_question_count
-    saved_pool = Hash[user_pool.map{|u| [u.id, {open_question_count: u.open_question_count, last_question_assigned_at: u.last_question_assigned_at}]}]
     self.create(question: log_values[:question], user: log_values[:user], group: log_values[:group], pool_floor: pool_floor, reason: log_values[:reason], pool: saved_pool )
+  end
+
+  def self.mapped_user_pool(user_pool)
+    Hash[user_pool.map{|u| [u.id, {open_question_count: u.open_question_count, last_question_assigned_at: u.last_question_assigned_at, routing_instructions: u.routing_instructions, auto_route: u.auto_route}]}]
   end
 
 end
