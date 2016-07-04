5aa5476d99c46b9e7c09b067b991c7f1cac493e9
Jason Adam Young <jayoung@extension.org>
2016-07-04 10:06:20 -0400

    reorganize query because of bad data

--- db/migrate/20160612212655_cleanup_group_connections.rb
+++ db/migrate/20160612212655_cleanup_group_connections.rb
@@ -1,12 +1,13 @@
 class CleanupGroupConnections < ActiveRecord::Migration
   def up
+    execute("UPDATE group_connections SET created_at = '2012-12-03 12:04:00' where created_at < '2012-12-03 12:04:00'")
+
     remove_column(:group_connections,:connection_code)
     remove_column(:group_connections,:send_notifications)
     remove_column(:group_connections, :connected_by)
 
     # data cleanup
     execute("DELETE from group_connections WHERE connection_type NOT IN('leader','member')")
-    execute("UPDATE group_connections SET created_at = '2012-12-03 12:04:00' where created_at < '2012-12-03 12:04:00'")
 
   end
 end
