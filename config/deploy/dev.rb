d4179ba2f477c2f462a73b6c53c90efdfffdc8c8
Jason Adam Young <jayoung@extension.org>
2016-09-23 12:38:53 -0400

    aws transition tasks

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -4,5 +4,5 @@ if(branch = ENV['BRANCH'])
 else
   set :branch, 'master'
 end
-set :vhost, 'dev-ask.extension.org'
+set :vhost, 'dev-ask.awsi.extension.org'
 server vhost, :app, :web, :db, :primary => true
