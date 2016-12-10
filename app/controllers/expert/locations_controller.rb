a30b60b3a237185ebb22e26885dd05d50716c9f0
Ben MacNeill <ben.macneill@gmail.com>
2016-12-10 09:40:30 -0500

    First stab at form for adding a primary group

--- app/controllers/expert/locations_controller.rb
+++ app/controllers/expert/locations_controller.rb
@@ -21,8 +21,33 @@ class Expert::LocationsController < ApplicationController
     @group_total_count = Group.with_expertise_location(@location.id).count
   end
 
-  def primarize
+  def primary_groups
     @location = Location.find_by_id(params[:id])
+  end
+
+  def add_primary_group
+    location = Location.find_by_id(params[:id])
+    group = Group.find_by_id(params[:group_id])
+
+    # when adding location, start out with the all county selection
+    group.expertise_counties << location.get_all_county unless group.expertise_counties.include?(location.get_all_county)
+
+    if !group.expertise_locations.include?(location)
+      group.expertise_locations << location
+    end
+
+    gl = group.group_locations.where("location_id = ?", location.id)
+    @group_location = GroupLocation.find(gl.first.id)
+    @group_location.update_attributes({:is_primary => true})
+
+    change_hash = Hash.new
+    change_hash[:expertise_locations] = {:old => "", :new => location.name}
+    UserEvent.log_added_location(group, current_user, change_hash)
+
+    return redirect_to expert_locations_path
+  end
+
+  def remove_primary_group
   end
 
 end
