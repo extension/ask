a3c4b9caf5730100c7477a8f96292d9d157df829
Jason Adam Young <jayoung@extension.org>
2017-01-19 10:46:08 -0500

    Sigh, file header

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,10 @@
+# === COPYRIGHT:
+#  Copyright (c) North Carolina State University
+#  Developed with funding for the National eXtension Initiative.
+# === LICENSE:
+#
+#  see LICENSE file
+
 class Group < ActiveRecord::Base
 
   include TagUtilities
