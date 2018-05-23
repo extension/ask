e5180c0c49c82f9d3896f0e3b1dc574e801479bd
Jason Young <jay@outfielding.net>
2018-05-23 11:00:42 -0400

    quote them for good measure and consistency

--- db/migrate/20180522183758_cleanup_data_items.rb
+++ db/migrate/20180522183758_cleanup_data_items.rb
@@ -1,11 +1,11 @@
 class CleanupDataItems < ActiveRecord::Migration
   def up
-    drop_table(:ratings)
-    drop_table(:notification_exceptions)
+    drop_table('ratings')
+    drop_table('notification_exceptions')
     remove_column(:mailer_caches, :open_count)
-    drop_table(:comments)
-    drop_table(:question_viewlogs)
-    drop_table(:activity_logs)
+    drop_table('comments')
+    drop_table('question_viewlogs')
+    drop_table('activity_logs')
     drop_table('old_evaluation_answers')
   end
 
