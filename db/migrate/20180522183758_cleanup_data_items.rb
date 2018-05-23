9d7b9027bb960cfc0350d7f93aa8b537176b0991
Jason Young <jay@outfielding.net>
2018-05-23 10:36:21 -0400

    remove view log activity (email open logging stopped 2013)

--- db/migrate/20180522183758_cleanup_data_items.rb
+++ db/migrate/20180522183758_cleanup_data_items.rb
@@ -4,8 +4,8 @@ class CleanupDataItems < ActiveRecord::Migration
     drop_table(:notification_exceptions)
     remove_column(:mailer_caches, :open_count)
     drop_table(:comments)
-    
     drop_table(:question_viewlogs)
+    drop_table(:activity_logs)
   end
 
   def down
